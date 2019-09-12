e05b6d0eae5506e8b8060541eea85a22e8a53ca5
Ahmad Mustafa <ahmadomar65@gmail.com>
2019-09-12 16:18:01 +0300

    remove tableView

--- JSON Task/Controller/UserByIdVC.swift
+++ JSON Task/Controller/UserByIdVC.swift
@@ -10,15 +10,7 @@ import UIKit
 
 class UserByIdVC: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var collectionView: UICollectionView!
-    var users: [Post] = [] {
-        didSet {
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }
-    }
     var userBySectionNumber = [(key: Int, value: [Post])]()
     var userById = [(key: Int, value: [Post])]()
     var userCollection: [Post] = [] {
@@ -31,7 +23,8 @@ class UserByIdVC: UIViewController {
     
     var selectedTilteById: String?
     var selectedBodyById: String?
-    var switchBTN = false
+    var switchBTN = true
+    var sizeWidth,sizeHeight: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,10 +34,10 @@ class UserByIdVC: UIViewController {
     
     // MARK: - ConfigureView
     private func configureView() {
-        tableView.delegate = self
-        tableView.dataSource = self
         collectionView.delegate = self
         collectionView.dataSource = self
+        sizeWidth = collectionView.frame.width
+        sizeHeight = collectionView.frame.height/4
         let url = "https://jsonplaceholder.typicode.com/posts"
         fetchUsers(using: url)
     }
@@ -55,65 +48,19 @@ class UserByIdVC: UIViewController {
     // MARK: - Switch Button
     @IBAction func switchBTN(_ sender: UIBarButtonItem) {
         switchBTN = !switchBTN
+        collectionView.reloadData()
         if switchBTN == false{
-            tableView.isHidden = true
-            collectionView.isHidden = false
+            sizeWidth = (collectionView.frame.width - 15)/2
+            sizeHeight = (collectionView.frame.width - 15)/2
         }else{
-            tableView.isHidden = false
-            collectionView.isHidden = true
+            sizeWidth = collectionView.frame.width
+            sizeHeight = collectionView.frame.height/4
         }
-    }
-}
-
-//MARK: Delegate & Data srouce for TableView
-extension UserByIdVC: UITableViewDelegate,UITableViewDataSource {
-    // MARK: Load data to the user
-     func load(with users: [Post]) -> Void{
-        userBySectionNumber = Dictionary(grouping: users, by: { user in
-            user.userId}).sorted(by: { $0.0 < $1.0})
-        print(userBySectionNumber)
-        DispatchQueue.main.async {
-            self.tableView.reloadData()
-        }
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return userBySectionNumber.count
-        }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return userBySectionNumber[section].value.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section Number \(userBySectionNumber[section].key)"
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "users",for: indexPath) as? customeCellTableViewCell{
-            let user = userBySectionNumber[indexPath.section].value[indexPath.row]
-            cell.dataModel(forModel: user)
-         return cell
-        }
-        return UITableViewCell()
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 99.95
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let path = tableView.indexPathForSelectedRow
-        let cell = userBySectionNumber[indexPath.section].value[indexPath.row]
-        tableView.reloadData()
-//        print(cell)
-        configureValues(with: cell)
-        performSegue(withIdentifier: "viewAllDetails", sender: path)
     }
 }
 
 //MARK: Delegate & Data srouce for CollectionView
-extension UserByIdVC: UICollectionViewDelegate, UICollectionViewDataSource{
+extension UserByIdVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return userCollection.count
     }
@@ -134,6 +81,10 @@ extension UserByIdVC: UICollectionViewDelegate, UICollectionViewDataSource{
         configureValues(with: cell)
         performSegue(withIdentifier: "viewAllDetails", sender: path)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: sizeWidth!, height: sizeHeight!)
+    }
 }
 
 // MARK: APIS
@@ -145,8 +96,6 @@ extension UserByIdVC {
             guard let data = data else {return}
             do{
                 let userFetch = try JSONDecoder().decode([Post].self, from: data)  // decode * ( Codable )
-                self.users = userFetch
-                self.load(with: userFetch)
                 self.userCollection = userFetch
                 DispatchQueue.main.async {
                     self.collectionView.reloadData()
